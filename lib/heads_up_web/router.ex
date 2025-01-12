9a77bd16ce781e2d44fbba5514eb9e926dabc5de
Alex <alexvdberg010109@gmail.com>
2025-01-12 22:00:19 +0200

    feat(plug): implemented a basic plug that assigns a basic key value pair to the conn

--- lib/heads_up_web/router.ex
+++ lib/heads_up_web/router.ex
@@ -8,10 +8,21 @@ defmodule HeadsUpWeb.Router do
     plug :put_root_layout, html: {HeadsUpWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :scoop
   end
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  def scoop(conn, _opts) do
+    answer = ~w(yes No Maybe) |> Enum.random()
+
+    conn = assign(conn, :answer, answer)
+
+    IO.inspect(conn)
+
+    conn
   end
 
   scope "/", HeadsUpWeb do
@@ -21,9 +32,9 @@ defmodule HeadsUpWeb.Router do
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", HeadsUpWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", HeadsUpWeb do
+    pipe_through :api
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:heads_up, :dev_routes) do
